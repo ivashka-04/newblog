84d26ac92c0de32aacde51154810b87b32cccb5d
Ivashka-04 <iskatel04@gmail.com>
2016-06-21 11:07:56 +0600

    Подключил пдо

--- views/view.php
+++ views/view.php
@@ -1,3 +1,14 @@
 <?php
+include __DIR__.'/../functions/bd.php';
 
 
+$stmt = $pdo->query('SELECT * FROM cinema ORDER BY data ASC');
+while ($row = $stmt->fetch())
+{
+    echo $row['title'] . "\n";
+    echo $row['content'] . "\n";
+    echo $row['genre'] . "\n";
+    echo $row['data'] . "\n";
+    echo $row['img'] . "\n";
+}
+?>
