576454030bc5d65664bb9557d8bc9186ea82d87e
Ivashka-04 <iskatel04@gmail.com>
2016-06-21 11:12:59 +0600

    Подключил базу через PDO

---
+++ functions/bd.php
@@ -0,0 +1,13 @@
+<?php
+$user ='mybd_user';
+$pass = '1234';
+
+$dsn = "mysql:host=localhost;dbname=test;charset=utf8";
+$opt = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+
+?>
